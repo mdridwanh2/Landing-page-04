fae23d3f8652da3ede683218507bb8d132f27182
Ridwan <mdridwan8020@gmail.com>
2024-03-23 23:16:01 +0600

    adding some cnt

--- javascript/main_javascipt.js
+++ javascript/main_javascipt.js
@@ -151,6 +151,40 @@ $(`.gallery`).magnificPopup({
 })
 
 
+$(document).ready(function() {
+    $('.play-video').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+          patterns: {
+            youtube: {
+              index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+              id: 'v=', // String that splits URL in a two parts, second part should be %id%
+              // Or null - full URL will be returned
+              // Or a function that should return %id%, for example:
+              // id: function(url) { return 'parsed id'; }
+
+              src: 'https://www.youtube.com/embed/5hPtU8Jbpg0?si=QluNJovi3IY-rnjM' // URL that will be set as a source for iframe.
+            },
+
+            // you may add here more sources
+
+          },
+
+          srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+        }
+    });
+});
+
+
+
+
+
 
 
 
@@ -203,3 +237,8 @@ $(`.gallery`).magnificPopup({
                 }
             }, duration);
         })
+
+
+
+
+
